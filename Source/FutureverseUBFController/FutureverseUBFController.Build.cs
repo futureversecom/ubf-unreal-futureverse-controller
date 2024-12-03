5c661a3f75e1bf11059dd126402005ef38fd520c
Mike Lee <mike.lee@futureverse.com>
2024-12-03 14:35:41 +1300

    chore: forgot to add DeveloperSettings dependency

--- Source/FutureverseUBFController/FutureverseUBFController.Build.cs
+++ Source/FutureverseUBFController/FutureverseUBFController.Build.cs
@@ -40,6 +40,7 @@ public class FutureverseUBFController : ModuleRules
 				"SlateCore",
 				"Json",
 				"JsonUtilities",
+				"DeveloperSettings"
 				// ... add private dependencies that you statically link with here ...	
 			}
 			);
