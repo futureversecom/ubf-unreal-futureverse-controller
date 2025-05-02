8361077ca9d616a9d84e65ab1dc1333ffd68f8ce
Mike Lee <mike.lee@futureverse.com>
2025-05-02 15:33:43 +1200

    fix: crash from invalid json object

--- Source/FutureverseUBFController/Public/MetadataJsonUtils.h
+++ Source/FutureverseUBFController/Public/MetadataJsonUtils.h
@@ -19,10 +19,13 @@ namespace MetadataJsonUtils
 
 	inline FString GetAssetName(const TSharedPtr<FJsonObject>& JsonObject)
 	{
+		FString AssetName;
+		
 		// Check if the main JSON object exists
 		if (!JsonObject.IsValid())
 		{
 			UE_LOG(LogTemp, Warning, TEXT("MetadataJsonUtils::TryGetAssetID Invalid JsonObject in InventoryItem"));
+			return AssetName;
 		}
 
 		// Check if the "node" field exists
@@ -31,6 +34,7 @@ namespace MetadataJsonUtils
 		{
 			UE_LOG(LogFutureverseUBFController, Warning, TEXT("MetadataJsonUtils::TryGetAssetID Missing or invalid 'node' field in JsonObject"));
 			LogJsonString(JsonObject);
+			return AssetName;
 		}
 
 		// Check if the "metadata" field exists
@@ -39,6 +43,7 @@ namespace MetadataJsonUtils
 		{
 			UE_LOG(LogFutureverseUBFController, Warning, TEXT("MetadataJsonUtils::TryGetAssetID Missing or invalid 'metadata' field in node object"));
 			LogJsonString(JsonObject);
+			return AssetName;
 		}
 
 		// Check if the "properties" field exists
@@ -47,60 +52,60 @@ namespace MetadataJsonUtils
 		{
 			UE_LOG(LogFutureverseUBFController, Warning, TEXT("MetadataJsonUtils::TryGetAssetID Missing or invalid 'properties' field in metadata object"));
 			LogJsonString(JsonObject);
+			return AssetName;
 		}
 
 		// Check if the "name" field exists and retrieve its value
-		FString AssetName;
 		if (!(*PropertiesObject)->TryGetStringField(TEXT("name"), AssetName) || AssetName.IsEmpty())
 		{
 			UE_LOG(LogFutureverseUBFController, Verbose, TEXT("MetadataJsonUtils::TryGetAssetID Missing or empty 'name' field in properties object"));
-			// LogJsonString(JsonObject);
 		}
-
-		// Safely generate the asset name
+		
 		return AssetName;
 	}
 
 	inline FString GetCollectionID(const TSharedPtr<FJsonObject>& JsonObject)
 	{
+		FString CollectionId;
 		// Check if the "node" field exists
 		const TSharedPtr<FJsonObject>* NodeObject;
 		if (!JsonObject->TryGetObjectField(TEXT("node"), NodeObject) || !NodeObject->IsValid())
 		{
 			UE_LOG(LogFutureverseUBFController, Warning, TEXT("MetadataJsonUtils::GetCollectionID Missing or invalid 'node' field in JsonObject"));
 			LogJsonString(JsonObject);
+			return CollectionId;
 		}
 		
 		// Check if the "collectionId" field exists and retrieve its value
-		FString CollectionId;
-		if (!(*NodeObject)->TryGetStringField(TEXT("collectionId"), CollectionId) || CollectionId.IsEmpty())
+		if (!NodeObject->Get()->TryGetStringField(TEXT("collectionId"), CollectionId) || CollectionId.IsEmpty())
 		{
 			UE_LOG(LogFutureverseUBFController, Warning, TEXT("MetadataJsonUtils::GetCollectionID Missing or empty 'collectionId' field in node object"));
-			LogJsonString(JsonObject);
 		}
-	
+		
 		return CollectionId;
 	}
 	
 	inline FString GetMetadataJson(const TSharedPtr<FJsonObject>& JsonObject)
 	{
+		FString MetadataJson;
+		
 		// Check if the "node" field exists
 		const TSharedPtr<FJsonObject>* NodeObject;
 		if (!JsonObject->TryGetObjectField(TEXT("node"), NodeObject) || !NodeObject->IsValid())
 		{
 			UE_LOG(LogFutureverseUBFController, Warning, TEXT("MetadataJsonUtils::GetMetadataJson Missing or invalid 'node' field in JsonObject"));
 			LogJsonString(JsonObject);
+			return MetadataJson;
 		}
 		
 		// Check if the "metadata" field exists
 		const TSharedPtr<FJsonObject>* MetadataObject;
-		if (!(*NodeObject)->TryGetObjectField(TEXT("metadata"), MetadataObject) || !MetadataObject->IsValid())
+		if (!NodeObject->Get()->TryGetObjectField(TEXT("metadata"), MetadataObject) || !MetadataObject->IsValid())
 		{
 			UE_LOG(LogFutureverseUBFController, Warning, TEXT("MetadataJsonUtils::GetMetadataJson Missing or invalid 'metadata' field in node object"));
 			LogJsonString(JsonObject);
 		}
 		
-		FString MetadataJson;
 		const TSharedRef<TJsonWriter<>> Writer = TJsonWriterFactory<>::Create(&MetadataJson);
 		FJsonSerializer::Serialize(MetadataObject->ToSharedRef(), Writer);
 	
